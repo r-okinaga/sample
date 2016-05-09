469055284d7d632ff389f5f49ae268f6fb784110
r-okinaga <Hrnn0PDk1>
2016-05-10 08:16:39 +0900

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
 
     before_save :downcase_email
     before_create :create_activation_digest
@@ -44,6 +44,18 @@ class User < ActiveRecord::Base
         UserMailer.account_activation(self).deliver_now
     end
 
+    #パスワード再設定の属性を設定する
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    #パスワード再設定のメールを送信する
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
     #渡されたトークンがダイジェストと一致したらtrueを返す
     def authenticated?(attribute, token)
         digest = send("#{attribute}_digest")
@@ -54,6 +66,11 @@ class User < ActiveRecord::Base
     #ユーザーログインを破棄する
     def forget
         update_attribute(:remember_digest, nil)
+    end
+
+    # パスワード再設定の期限が切れている場合はtrueを返す
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
     end
 
     private
